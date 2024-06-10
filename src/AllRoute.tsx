31f111e34c067c5136ef17b1f1cdecd89217304a
CodeStar2244 <codestar2244@gmail.com>
2024-06-10 22:27:52 +0530

    import changes

--- src/AllRoute.tsx
+++ src/AllRoute.tsx
@@ -1,28 +1,31 @@
 import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import DeliveryManagement from "./pages/DeliveryManagement";
 import FleedManagement from "./pages/FleetManagement";
 import Layout from "./pages/Layout";
 import LoadingSheet from "./pages/LoadingSheet";
 import OrderTracking from "./pages/OrderTracking";
 import RouteCreation from "./pages/RouteCreation";
+import Dashboard from "./pages/dashboard";
 
 function AllRoute() {
-  return (
-    <BreadcrumbsProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/loading-sheets" element={<LoadingSheet />} />
-          <Route path="/fleet-management" element={<FleedManagement />} />
-          <Route path="/delivery" element={<DeliveryManagement />} />
-          <Route path="/order-tracking" element={<OrderTracking />} />
-          <Route path="/route-creation" element={<RouteCreation />} />
-        </Route>
-      </Routes>
-    </BreadcrumbsProvider>
-  );
+    return (
+        <BreadcrumbsProvider>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/loading-sheets" element={<LoadingSheet />} />
+                    <Route
+                        path="/fleet-management"
+                        element={<FleedManagement />}
+                    />
+                    <Route path="/delivery" element={<DeliveryManagement />} />
+                    <Route path="/order-tracking" element={<OrderTracking />} />
+                    <Route path="/route-creation" element={<RouteCreation />} />
+                </Route>
+            </Routes>
+        </BreadcrumbsProvider>
+    );
 }
 
 export default AllRoute;
